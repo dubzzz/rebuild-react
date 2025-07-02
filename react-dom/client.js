879006180969a66669ed2698ad9fb179a287598d
Nicolas DUBIEN <github@dubien.org>
2025-07-02 12:01:14 +0200

    End commit

--- react-dom/client.js
+++ react-dom/client.js
@@ -5,42 +5,68 @@
  */
 
 /**
- *
- * @param {HTMLElement} parentElement
- * @param {JSX} component
+ * @param {Fiber} fiber
  */
-function render(parentElement, component) {
-  if (component.type == "text") {
-    const element = document.createTextNode(component.props.children);
-    parentElement.appendChild(element);
-    return;
-  }
-  const element = document.createElement(component.type);
-  const children = Array.isArray(component.props.children)
-    ? component.props.children
-    : component.props.children != null
-    ? [component.props.children]
+function traverse(fiber) {
+  fiber.children = Array.isArray(fiber.props.children)
+    ? fiber.props.children
+    : fiber.props.children != null
+    ? [fiber.props.children]
     : [];
-  for (const child of children) {
-    const revampedChild =
-      typeof child === "string"
-        ? { type: "text", props: { children: [child] } }
-        : child;
-    render(element, revampedChild);
+  for (let index = 0; index !== fiber.children.length; ++index) {
+    const child = fiber.children[index];
+    if (typeof child === "string") {
+      fiber.children[index] = {
+        type: "text",
+        props: {},
+        children: [],
+        parent: fiber,
+        dom: undefined,
+        content: child,
+      };
+    }
   }
-  parentElement.appendChild(element);
+  for (const child of fiber.children) {
+    child.parent = fiber;
+    traverse(child);
+  }
 }
 
 /**
- * @param {HTMLElement} rootElement
+ * @param {Fiber} fiber
  */
+function commit(fiber) {
+  const parentDom = fiber.parent.dom;
+  if (!fiber.dom) {
+    fiber.dom =
+      fiber.type === "text"
+        ? document.createTextNode(fiber.content)
+        : document.createElement(fiber.type);
+  }
+  parentDom.appendChild(fiber.dom);
+  for (const child of fiber.children) {
+    console.log({ child });
+    commit(child);
+  }
+}
+
+/** @typedef {JSX & { children?: Fiber[]; parent?: Fiber; dom?: HTMLElement }} Fiber */
+
 export const createRoot = (rootElement) => {
   return {
     /**
      * @param {JSX} component
      */
     render: (component) => {
-      render(rootElement, component);
+      /** @type {Fiber} */
+      const rootFiber = {
+        type: "root",
+        props: { children: [component] },
+        children: [component],
+        dom: rootElement,
+      };
+      traverse(rootFiber);
+      commit(rootFiber.children[0]);
     },
   };
 };
