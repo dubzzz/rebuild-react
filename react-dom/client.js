863354c2228aa21b3584a81a6f633b5f852c6dc0
Nicolas DUBIEN <github@dubien.org>
2025-07-02 11:28:25 +0200

    End root

--- react-dom/client.js
+++ react-dom/client.js
@@ -1,7 +1,46 @@
 // @ts-check
 
-export const createRoot = () => {
+/**
+ * @typedef {{ type: string; props: object }} JSX
+ */
+
+/**
+ *
+ * @param {HTMLElement} parentElement
+ * @param {JSX} component
+ */
+function render(parentElement, component) {
+  if (component.type == "text") {
+    const element = document.createTextNode(component.props.children);
+    parentElement.appendChild(element);
+    return;
+  }
+  const element = document.createElement(component.type);
+  const children = Array.isArray(component.props.children)
+    ? component.props.children
+    : component.props.children != null
+    ? [component.props.children]
+    : [];
+  for (const child of children) {
+    const revampedChild =
+      typeof child === "string"
+        ? { type: "text", props: { children: [child] } }
+        : child;
+    render(element, revampedChild);
+  }
+  parentElement.appendChild(element);
+}
+
+/**
+ * @param {HTMLElement} rootElement
+ */
+export const createRoot = (rootElement) => {
   return {
-    render: () => {},
+    /**
+     * @param {JSX} component
+     */
+    render: (component) => {
+      render(rootElement, component);
+    },
   };
 };
