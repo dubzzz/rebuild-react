5d26c8c379d1bdaab4753394f08f2746dea1aa4b
Nicolas DUBIEN <github@dubien.org>
2025-07-02 11:13:43 +0200

    End JSX

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,5 +1,18 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import "./main.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <div>
+    <div>
+      Hello{" "}
+      <span>
+        <i>Enter your name</i>
+      </span>
+    </div>
+    <input
+      value=""
+      placeholder="Your name"
+      onKeyDown={(event) => console.log(event)}
+    />
+  </div>
+);
--- react/index.js
+++ react/index.js
@@ -1,18 +1,1 @@
-globalThis.currentFiber = undefined;
-
 export default {};
-export const useState = (initialValue) => {
-  const fiber = globalThis.currentFiber;
-  if (fiber.hookInitMode) {
-    const state = [
-      initialValue,
-      (newValue) => {
-        state[0] = newValue;
-        fiber.rerender();
-      },
-    ];
-    fiber.hooks[fiber.hookIndex++] = state;
-    return state;
-  }
-  return fiber.hooks[fiber.hookIndex++];
-};
